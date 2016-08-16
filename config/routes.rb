252a8faf89f43b96fa85d9e5c0c4e65012116b85
margarita itaj <itaimargi@hotmail.com>
2016-08-16 17:31:25 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   devise_for :employees
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :contractor do
+    resource :profile, only: [:edit, :update]
+    resources :jobs, only: [:index, :show] do
+      resources :job_applications, only: [:new, :create]
+    end
+  end
 end
