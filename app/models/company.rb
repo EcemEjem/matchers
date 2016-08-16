bc1576d69015a9d73748282d9db570af062bb695
Charles <c.depoortere28@gmail.com>
2016-08-16 11:54:52 +0200

    add models and validations and relations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,11 @@
 class Company < ApplicationRecord
+    has_many :job_offers
+    has_many :job_applications, through: :job_offers
+
+    validate :name, presence: true
+    validate :industry, presence: true
+    validate :email, presence: true
+    validate :description, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
