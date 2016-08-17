d8b5b9d8332dc147a39d197028e72bfaaea31c6b
Ecem Erseker <ecem.erseker@gmail.com>
2016-08-17 11:37:21 +0200

    Work in progress for Controllers and Views

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :contractor do
-    resource :profile, only: [:edit, :update]
+    resource :profile, only: [:edit, :update]''
     resources :jobs, only: [:index, :show] do
       resources :job_applications, only: [:new, :create]
     end
