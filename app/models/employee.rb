9f243530c92b43d33ec73be02cd8d77a79d0646d
margarita itaj <itaimargi@hotmail.com>
2016-08-17 16:00:03 +0200

    style to home page

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,11 +2,10 @@ class Employee < ApplicationRecord
   has_many :job_applications
   has_many :job_offers, through: :job_applications
 
-  validates :first_name,     presence: true
-  validates :last_name,      presence: true
-  validates :email,          presence: true
-  validates :gender,         presence: true
-  validates :age,            presence: true
+  validates :first_name,     presence: true, on: :update
+  validates :last_name,      presence: true, on: :update
+  validates :gender,         presence: true, on: :update
+  validates :age,            presence: true, on: :update
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
