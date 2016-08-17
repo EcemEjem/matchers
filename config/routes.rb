0f551821dc451425e707c983fe8f8613ffe817d4
Ecem Erseker <ecem.erseker@gmail.com>
2016-08-17 12:28:08 +0200

    Update controllers views structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  get 'job_applications/new'
-
-  get 'job_applications/create'
-
   devise_for :companies
   devise_for :employees
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :contractor do
-    resource :profile, only: [:edit, :update]''
+    resource :profile, only: [:edit, :update]
     resources :jobs, only: [:index, :show] do
       resources :job_applications, only: [:new, :create]
     end
