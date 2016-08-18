d4277773283eafedfc39f5cff80e3159d390db95
margarita itaj <itaimargi@hotmail.com>
2016-08-18 22:50:19 +0200

    fix style and add photo

--- app/controllers/contractor/jobs_controller.rb
+++ app/controllers/contractor/jobs_controller.rb
@@ -1,6 +1,7 @@
 class Contractor::JobsController < ApplicationController
   #list all the job offer
   def index
+    @a = 1
     if params[:location] != ""
       @jobs = JobOffer.where(city: params[:location])
     else
