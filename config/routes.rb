131c3bd3ffb8475d3d63eea7335fef1082c43f91
Ecem Erseker <ecem.erseker@gmail.com>
2016-08-19 16:23:06 +0200

    Contractor-Account-Profiles-JobList

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :contractor do
     root to: 'cities#index', as: :root
-    resource :profile, only: [:edit, :update]
+
+    namespace :account do
+      resource :profile, only: [:show, :edit, :update]
+
+      resources :job_applications, only: [:index]
+    end
+
     resources :jobs, only: [:index, :show] do
       resources :job_applications, only: [:new, :create]
     end
