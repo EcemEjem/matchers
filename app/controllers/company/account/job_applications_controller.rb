b64d46df028e4b25949b6e822fedff419850514c
Ecem Erseker <ecem.erseker@gmail.com>
2016-08-29 12:19:51 +0200

    Correct all minor backend issues and connecting frontend issues like navbar page connections

--- app/controllers/company/account/job_applications_controller.rb
+++ app/controllers/company/account/job_applications_controller.rb
@@ -1,15 +1,21 @@
 class Company::Account::JobApplicationsController < Company::BaseController
 
   def show
-    @job_application = current_company.job_applications.find(params[:id])
+    @job_application = JobApplication.find(params[:id])
     @job_application.read_at = Time.now
     @job_application.save!
   end
 
   def accept
+    @job_application = JobApplication.find(params[:id])
+    @job_application.update(status: "accepted")
+    redirect_to company_account_job_application_path(@job_application)
   end
 
   def decline
+    @job_application = JobApplication.find(params[:id])
+    @job_application.update(status: "declined")
+    redirect_to company_account_job_application_path(@job_application)
   end
 
 end
