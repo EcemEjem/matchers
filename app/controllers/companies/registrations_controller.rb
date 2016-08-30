1c9817d743b011bc3b113bd82cf8f0fb1e1fa2ef
margarita itaj <itaimargi@hotmail.com>
2016-08-30 20:37:17 +0200

    fix login redirect on company sing_in in company side

--- app/controllers/companies/registrations_controller.rb
+++
@@ -1,6 +0,0 @@
-class RegistrationsController < Device::RegistrationsController
-  def sign_up_params
-     params.require(:company).permit(:name, :industry, :description, :photo, :photo_cache)
-  end
-
-end
