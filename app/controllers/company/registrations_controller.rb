64bcf5d71920123367221fc03ceaacf32c7f2696
Charles <c.depoortere28@gmail.com>
2016-09-01 12:21:30 +0200

    Add company redirection after sign up

--- app/controllers/company/registrations_controller.rb
+++ app/controllers/company/registrations_controller.rb
@@ -1,6 +1,13 @@
-
-  class Company::RegistrationsController < Devise::RegistrationsController
-    def sign_up_params
-       params.require(:company).permit(:name, :industry, :description, :email, :password, :photo, :photo_cache)
-    end
+class Company::RegistrationsController < Devise::RegistrationsController
+  def sign_up_params
+     params.require(:company).permit(:name, :industry, :description, :email, :password, :photo, :photo_cache)
   end
+
+  def after_sign_up_path_for(resource)
+    company_account_profile_path
+  end
+
+  # def after_sign_out_path_for(resource)
+
+  # end
+end
